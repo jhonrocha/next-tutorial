4f7c4e7edeec02b394cb0db2c4778053733d3c5b
Jhon Rocha <jhmrocha@gmail.com>
2024-01-02 00:14:53 -0300

    Adding ShadCn

---
+++ app/playground/page.tsx
@@ -0,0 +1,11 @@
+export default function Playground() {
+  return (
+    <div>
+      <h1>Playground</h1>
+      <ol>
+        <li>Shad.cn</li>
+        <li>Clerk</li>
+      </ol>
+    </div>
+  );
+}
